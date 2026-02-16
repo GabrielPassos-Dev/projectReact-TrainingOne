1c3ffe0b7b73bd640fa0c71d670d2d334ed84d85
Gabriel Passos DEV <gabrielpassos.ti@gmail.com>
2026-02-16 12:55:54 -0300

    Adjusting the back and finish buttons.

--- projectFile/src/components/FinishShoppingCart.jsx
+++ projectFile/src/components/FinishShoppingCart.jsx
@@ -27,7 +27,7 @@ function FinishShoppingCart({ total, setCart }) {
           to="/"
           className=" w-25 text-center
           px-7 py-2.5 bg-white text-blue-600 border-2 border-blue-600 font-semibold rounded-lg shadow-md hover:bg-blue-800 hover:shadow-l hover:text-white transition-all duration-300
-          focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+          focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 flex items-center justify-center"
         >
           Voltar
         </Link>
@@ -35,7 +35,7 @@ function FinishShoppingCart({ total, setCart }) {
           onClick={() => finish()}
           className=" w-45 text-center
           px-7 py-2.5 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg transition-all duration-300
-          focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 cursor-pointer"
+          focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 cursor-pointer flex items-center justify-center"
         >
           Finalizar Compra
         </button>
